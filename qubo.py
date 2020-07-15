cf234da566ac6316866cb0a21ed748cad37065c8
ru35zaf <mueller.tobias1@campus.lmu.de>
2020-07-15 11:17:37 +0200

    updates

--- qubo.py
+++ qubo.py
@@ -1,10 +1,10 @@
 from pyqubo import Spin
 import neal
 
-s1, s2, s3, s4 = Spin("s1"), Spin("s2"), Spin("s3"), Spin("s4")
-H = (4*s1 + 2*s2 + 7*s3 + s4)**2
+s1, s2, s3, s4, s5, s6 = Spin("s1"), Spin("s2"), Spin("s3"), Spin("s4"), Spin("s5"), Spin("s6")
+H = -(s1 - s2 + s3*s4 - s4*s5)
 model = H.compile()
-qubo, offset=model.to_qubo()
+qubo, offset = model.to_qubo()
 
 sampler = neal.SimulatedAnnealingSampler()
 response = sampler.sample_qubo(qubo)
