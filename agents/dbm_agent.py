1e5dbc4dfd756398c62ef1530574f4335d03cab0
Tobias Müller <mueller.tobias1@campus.lmu.de>
2020-11-13 18:26:36 +0100

    tests

--- agents/dbm_agent.py
+++ agents/dbm_agent.py
@@ -50,11 +50,11 @@ class DBM_agent(nn.Module):
             a = [1, 1]
 
 
-        self.w += self.lr * (r + self.discount_factor * q) * np.outer(hh[0], s)
-        self.u += self.lr * (r + self.discount_factor * q) * np.outer(hh[-1], a)
+        self.w += self.lr * (r - self.discount_factor * q) * np.outer(hh[0], s)
+        self.u += self.lr * (r - self.discount_factor * q) * np.outer(hh[-1], a)
 
         for i in range(self.n_layers-1):
-            self.hh[i] += self.lr * (r + self.discount_factor * q) * np.outer(hh[i], hh[i+1])
+            self.hh[i] += self.lr * (r - self.discount_factor * q) * np.outer(hh[i], hh[i+1])
 
         return q
 
