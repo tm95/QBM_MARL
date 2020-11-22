0198a8df3d867f3ae364df9cb0b454f464958b7b
Tobias Müller <mueller.tobias1@campus.lmu.de>
2020-11-22 18:56:56 +0100

    tests

--- env_utils.py
+++ env_utils.py
@@ -107,8 +107,8 @@ class Env():
 		if agent_state_tuple == (0, 0):
 			reward = self.reward_function_tuple[agent_state_tuple[0]][agent_state_tuple[1]]
 		else:
-			reward = 0
-		reward = self.reward_function_tuple[agent_state_tuple[0]][agent_state_tuple[1]]
+			reward = -10
+		#reward = self.reward_function_tuple[agent_state_tuple[0]][agent_state_tuple[1]]
 		return reward
 
 	def get_available_actions(self, current_state):
