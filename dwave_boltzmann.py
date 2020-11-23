c2314089270b0fb3b921975aa0d8ec647a098184
Tobias Müller <mueller.tobias1@campus.lmu.de>
2020-11-23 09:14:28 +0100

    tests

--- dwave_boltzmann.py
+++ dwave_boltzmann.py
@@ -26,8 +26,8 @@ def run(env, agent, logger):
         step_count = 1
         fidelity_count = 1
         done = False
-        #action_index, old_position_tuple = agent.policy(agent_state_tuple, available_actions, available_actions_list)
-        #agent_state_tuple, available_actions, fidelity_count, reward, done = env.step(action_index, agent_state_tuple, old_position_tuple)
+        action_index, old_position_tuple = agent.policy(agent_state_tuple, available_actions, available_actions_list)
+        agent_state_tuple, available_actions, fidelity_count, reward, done = env.step(action_index, agent_state_tuple, old_position_tuple)
 
         while step_count < nb_steps and not done:
             # env.render(agent_state_tuple[0])
