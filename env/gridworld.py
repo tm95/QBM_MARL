3a51f9dd744d4ab22ec6bfe7650e3842024007e7
Tobias Müller <mueller.tobias1@campus.lmu.de>
2020-12-23 15:05:54 +0100

    tests

--- env/gridworld.py
+++ env/gridworld.py
@@ -79,7 +79,7 @@ class Env():
 				next_state = (current_state[i][0])
 
 			binary_agent[next_state[0]][next_state[1]] = 1
-			for goal in self.goals:
+			for goal in self.goals[i]:
 				binary_agent[goal[0]][goal[1]] = 1
 
 			for j in range(self.nb_agents):
@@ -99,8 +99,8 @@ class Env():
 					if n not in list(self.available_states):
 						n = (current_state[j][0])
 
-					#for goal in self.goals[j]:
-					#	binary_agent[goal[0]][goal[1]] = 2
+					for goal in self.goals[j]:
+						binary_agent[goal[0]][goal[1]] = 2
 
 					binary_agent[n[0]][n[1]] = 2
 
@@ -111,7 +111,7 @@ class Env():
 	def get_done(self):
 		done = []
 		for i in range(self.nb_agents):
-			if len(self.goals) == 0:
+			if len(self.goals[i]) == 0:
 				done.append(True)
 			else:
 				done.append(False)
@@ -120,17 +120,17 @@ class Env():
 	def get_reward(self, agent_state_tuple):
 		rewards = []
 		for i in range(self.nb_agents):
-			if len(self.goals) == 0:
+			if len(self.goals[i]) == 0:
 				reward = 0
-			elif agent_state_tuple[i][0] in list(self.goals):
+			elif agent_state_tuple[i][0] in list(self.goals[i]):
 				reward = 220
-				self.goals.remove(agent_state_tuple[i][0])
+				self.goals[i].remove(agent_state_tuple[i][0])
 			else:
 				reward = -10
-			#for j in range(self.nb_agents):
-			#	if i != j:
-			#		if agent_state_tuple[i][0] in list(self.goals[j]) and len(self.goals[i]) != 0:
-			#			reward = -210
+			for j in range(self.nb_agents):
+				if i != j:
+					if agent_state_tuple[i][0] in list(self.goals[j]) and len(self.goals[i]) != 0:
+						reward = -210
 
 			rewards.append(reward)
 
@@ -140,7 +140,7 @@ class Env():
 		obs = []
 		states = []
 		decimal = []
-		self.goals = [(0,0), (2,2)]
+		self.goals = [[(0,0)], [(2,2)]]
 		for i in range(self.height):
 			for j in range(self.width):
 				if (i,j) not in self.goals[0] and (i,j) not in self.goals[1]:
@@ -152,13 +152,13 @@ class Env():
 		for i in range(self.nb_agents):
 			binary = -np.ones((self.height, self.width), dtype=int)
 			binary[decimal[i][0]][decimal[i][1]] = 1
-			for goal in self.goals:
+			for goal in self.goals[i]:
 				binary[goal[0]][goal[1]] = 1
 			for j in range(self.nb_agents):
 				if i != j:
 					binary[decimal[j][0]][decimal[j][1]] = 2
-					#for goal in self.goals:
-					#	binary[goal[0]][goal[1]] = 2
+					for goal in self.goals[j]:
+						binary[goal[0]][goal[1]] = 2
 			obs.append((decimal[i], tuple(binary.flatten())))
 
 		return obs, self.available_actions_list
